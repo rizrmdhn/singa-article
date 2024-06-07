750a427cdc177d15a78bcaa1af75e38755af5f10
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-07 14:02:44 +0800

    refactor: Update error component to handle navigation history

--- src/components/component/error-component.tsx
+++ src/components/component/error-component.tsx
@@ -19,7 +19,14 @@ export function ErrorComponent({ error }: { error: string }) {
         </p>
         <a
           className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:cursor-pointer hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-          onClick={() => navigate(-1)}
+          onClick={() => {
+            // check if there is a previous page
+            if (window.history.length > 2) {
+              navigate(-1);
+            } else {
+              navigate("/articles");
+            }
+          }}
         >
           Go back to previous page
         </a>
