12cd18ad732e6437b606d22563aca12ae306c169
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-07 08:42:48 +0800

    chore: Remove unused ArticleSection component

---
+++ src/pages/GlobalArticlePage.tsx
@@ -0,0 +1,9 @@
+import { ArticleSection } from "@/components/component/article-section";
+
+export default function GlobalArticlePage() {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <ArticleSection />
+    </main>
+  );
+}
