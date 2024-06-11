2224a8828d3fd131e535a96991c4c8c388d80a1e
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-11 14:49:34 +0800

    refactor: Update article section to handle no articles found case and improve UI layout

--- src/components/component/article-section.tsx
+++ src/components/component/article-section.tsx
@@ -18,14 +18,25 @@ export function ArticleSection() {
             </p>
           </div>
         </div>
-        <div className="mx-auto grid gap-8 py-12 sm:grid-cols-2 lg:grid-cols-3">
-          {status === "pending"
-            ? Array(6)
-                .fill(0)
-                .map((_, index) => <ArticleCardLoader key={index} />)
-            : data?.map((article) => (
-                <ArticleCard key={article.id} {...article} />
-              ))}
+        <div className="mt-8 flex flex-row flex-wrap justify-center gap-4">
+          {status === "pending" ? (
+            Array(6)
+              .fill(0)
+              .map((_, index) => <ArticleCardLoader key={index} />)
+          ) : data?.length === 0 ? (
+            <div className="flex w-full flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                No Article Found
+              </h2>
+              <p className="max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Sorry, we couldn't find any article.
+              </p>
+            </div>
+          ) : (
+            data?.map((article) => (
+              <ArticleCard key={article.id} {...article} />
+            ))
+          )}
         </div>
       </div>
     </section>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,28 @@
 import LoginForm from "@/components/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
+      <div className="flex flex-col gap-3 text-center">
+        <h1 className="text-3xl font-bold">
+          Welcome to Singa Article Dashboard App
+        </h1>
+        <p className="text-lg">
+          Please login to continue or go to article page
+        </p>
+        <p className="text-lg">
+          click{" "}
+          <a
+            onClick={() => navigate("/articles")}
+            className="text-blue-500 hover:cursor-pointer hover:text-blue-700 hover:underline"
+          >
+            here
+          </a>{" "}
+          to go to article page
+        </p>
+      </div>
       <LoginForm />
     </div>
   );
