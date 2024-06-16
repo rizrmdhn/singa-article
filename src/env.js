38a4339c7dee666688740eb28d81c9934e845845
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-16 15:03:54 +0800

    refactor: Remove unused NEXT_PUBLIC_API_URL variable

--- src/env.js
+++ src/env.js
@@ -22,7 +22,6 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_API_URL: z.string().url(),
   },
 
   /**
@@ -33,7 +32,6 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     GOOGLE_CLOUD_STORAGE_BUCKET_NAME:
       process.env.GOOGLE_CLOUD_STORAGE_BUCKET_NAME,
     GOOGLE_CLOUD_STORAGE_STORAGE_PATH:
