168b05e371d43112f77f410b5275f1268acc1eda
MuhammadRadifa <m.difa123456@gmail.com>
2024-06-18 09:23:40 +0800

    refactor: Update video section dimensions and YouTube video URL

--- src/components/component/Video.tsx
+++ src/components/component/Video.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function VideoSection() {
   return (
-    <div className="h-96 w-full p-10 md:h-[600px] md:w-2/3 md:p-20">
+    <div className="h-96 w-full p-10 md:h-[640px] md:w-2/3 md:p-20">
       <div id="about"></div>
       <motion.div
         whileInView={{ scale: 1 }}
@@ -13,9 +13,9 @@ export default function VideoSection() {
         viewport={{ once: true }}
         className="h-full w-full"
       >
-        <div className="md:shadow-blue-tertiary h-full w-full overflow-hidden rounded-xl md:shadow-2xl">
+        <div className="h-full w-full overflow-hidden rounded-xl md:shadow-2xl md:shadow-blue-tertiary">
           <iframe
-            src={"https://www.youtube.com/embed/Uer540qlGZc"}
+            src={"https://www.youtube.com/embed/XNhLLJ7GuhY"}
             title="YouTube video player"
             allowFullScreen
             className="h-full w-full"
