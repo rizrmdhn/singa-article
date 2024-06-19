666bde6cec72beb01bfb636b623c55ac8e45f1dd
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-19 09:17:19 +0800

    feat: rewrite infrastructure to nextjs

--- src/hooks/useArticle.ts
+++
@@ -1,9 +0,0 @@
-import { getArticles } from "@/lib/api";
-import { useQuery } from "@tanstack/react-query";
-
-export default function useArticle() {
-  return useQuery({
-    queryKey: ["articles"],
-    queryFn: getArticles,
-  });
-}
