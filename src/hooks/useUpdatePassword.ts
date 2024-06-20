75b053158cab5e223664efab83c3136658a68ad7
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-20 16:29:00 +0800

    refactor: Update password update form and server-side action

--- src/hooks/useUpdatePassword.ts
+++ src/hooks/useUpdatePassword.ts
@@ -1,16 +1,20 @@
 import { toast } from "@/components/ui/use-toast";
 import { updateUserPassword } from "@/server/actions/user-action";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export default function useUpdatePassword() {
+  const { push } = useRouter();
+
   return useMutation({
     mutationFn: updateUserPassword,
     onSuccess: (data) => {
       if (data?.data?.meta.status === "success") {
         toast({
           title: "Success",
-          description: "You have been logged in successfully",
+          description: "Password updated successfully",
         });
+        push("/dashboard/settings");
       } else {
         toast({
           title: "Error",
